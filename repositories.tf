809ebf863023a46a423aa79c95e69aed171122ba
Phil Dorczuk <pdorczuk@gmail.com>
2022-02-01 15:44:21 +0000

    refactor(repos): remove module structure

--- repositories.tf
+++ repositories.tf
@@ -1,13 +1,44 @@
 #######################################################################################################################
 # Create kubescreen repository
-module "kubescreen" {
-  source = "./tf-modules/github-repo.tf"
-  
-  name = kubescreen
+resource "github_repository" "kubescreen" {
+  name = "kubescreen"
+  description = "Deploy and secure a Kubernetes cluster using GitOps."
 }
 
-module "payment-app" {
-  source = "./tf-modules/github-repo.tf"
-  
-  name = payment-app
+# Add memberships for kube-apps repository
+resource "github_team_repository" "kubescreen" {
+  for_each = {
+    for team in local.repo_teams_files["kubescreen"] :
+    team.team_name => {
+      team_id    = github_team.all[team.team_name].id
+      permission = team.permission
+    } if lookup(github_team.all, team.team_name, false) != false
+  }
+
+  team_id    = each.value.team_id
+  repository = github_repository.kubescreen.id
+  permission = each.value.permission
+}
+
+
+#######################################################################################################################
+# Create payments repository
+resource "github_repository" "payments" {
+  name = "payments"
+  description = "Python app to test and demo CI."
+}
+
+# Add memberships for kube-apps repository
+resource "github_team_repository" "payments" {
+  for_each = {
+    for team in local.repo_teams_files["payments"] :
+    team.team_name => {
+      team_id    = github_team.all[team.team_name].id
+      permission = team.permission
+    } if lookup(github_team.all, team.team_name, false) != false
+  }
+
+  team_id    = each.value.team_id
+  repository = github_repository.payments.id
+  permission = each.value.permission
 }
